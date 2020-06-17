a5c55f20e2f039c11d093f736c096588eb143e5f
Patryk Gajek <pgajek94@gmail.com>
2020-06-17 14:16:07 +0200

    basic styles of game board

--- js.js
+++ js.js
@@ -2,6 +2,7 @@ const grid = document.querySelector('.grid');
 function createDivs() {
   for (let i = 0; i < 200; i++) {
     const newDiv = document.createElement('div');
+    newDiv.classList.add('grid__item');
     grid.appendChild(newDiv);
   }
 }
