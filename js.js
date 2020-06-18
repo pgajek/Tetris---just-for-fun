e1f392eb642056dc2bd67de1fb0250b67dccb94f
Patryk Gajek <pgajek94@gmail.com>
2020-06-18 12:59:16 +0200

    implemented moving and rotating puzzles

--- js.js
+++ js.js
@@ -1,7 +1,13 @@
 const grid = document.querySelector('.grid');
+const miniGrid = document.querySelector('.miniGrid');
 const scoreDisplay = document.querySelector('.score');
 const startBtn = document.querySelector('.start__button');
+
 const width = 10;
+let currentRotation = 0;
+let currentPosition = 4;
+let nextRandom = 0;
+
 for (let i = 0; i < 200; i++) {
   const newDiv = document.createElement('div');
   newDiv.classList.add('grid__item');
@@ -10,6 +16,12 @@ for (let i = 0; i < 200; i++) {
 for (let i = 0; i < 10; i++) {
   const newDiv = document.createElement('div');
   newDiv.classList.add('taken');
+  newDiv.classList.add('grid__item');
+  grid.appendChild(newDiv);
+}
+for (let i = 0; i < 16; i++) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('miniGrid__item');
   grid.appendChild(newDiv);
 }
 let squares = [...document.querySelectorAll('.grid__item')];
@@ -47,8 +59,7 @@ const iPuzzle = [
   [width, width + 1, width + 2, width + 3],
 ];
 const puzzles = [lPuzzle, zPuzzle, oPuzzle, iPuzzle, tPuzzle];
-let currentRotation = 0;
-let currentPosition = 4;
+
 let random = Math.floor(Math.random() * puzzles.length);
 let current = puzzles[random][currentRotation];
 
@@ -64,9 +75,101 @@ function undrawPuzzle() {
   });
 }
 let timerid = setInterval(moveDown, 1000);
+//
 
+function control(e) {
+  if (e.keyCode === 37) {
+    moveLeft();
+  } else if (e.keyCode === 38) {
+    rotate();
+  } else if (e.keyCode === 39) {
+    moveRight();
+  } else if (e.keyCode === 40) {
+    moveDown();
+  }
+}
+document.addEventListener('keydown', control);
 function moveDown() {
   undrawPuzzle();
   currentPosition += width;
   drawPuzzle();
+  freeze();
+}
+function freeze() {
+  if (
+    current.some((index) =>
+      squares[currentPosition + index + width].classList.contains('taken')
+    )
+  ) {
+    current.forEach((index) =>
+      squares[currentPosition + index].classList.add('taken')
+    );
+    random = Math.floor(Math.random() * puzzles.length);
+    current = puzzles[random][currentRotation];
+    currentPosition = 4;
+    drawPuzzle();
+  }
+}
+
+//moving
+function moveLeft() {
+  undrawPuzzle();
+  const isAtLeftEdge = current.some(
+    (index) => (currentPosition + index) % width === 0
+  );
+  if (!isAtLeftEdge) currentPosition -= 1;
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains('taken')
+    )
+  ) {
+    currentPosition += 1;
+  }
+  drawPuzzle();
+}
+function moveRight() {
+  undrawPuzzle();
+  const isAtRightEdge = current.some(
+    (index) => (currentPosition + index) % width === width - 1
+  );
+  if (!isAtRightEdge) currentPosition += 1;
+  if (
+    current.some((index) =>
+      squares[currentPosition + index].classList.contains('taken')
+    )
+  ) {
+    currentPosition -= 1;
+  }
+  drawPuzzle();
+}
+function rotate() {
+  undrawPuzzle();
+  currentRotation++;
+  if (currentRotation === current.length) {
+    currentRotation = 0;
+  }
+  current = puzzles[random][currentRotation];
+
+  drawPuzzle();
+}
+
+//
+
+const displaySquares = document.querySelectorAll('.miniGrid__item');
+const displayWidth = 4;
+let displayIndex = 0;
+
+const upNextPuzzles = [
+  [1, width + 1, width * 2 + 1, 2],
+  [0, width, width + 1, width * 2 + 1],
+  [1, width, width + 1, width + 2],
+  [0, 1, width, width + 1],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+];
+
+function displayShape() {
+  displaySquares.forEach((square) => {
+    square.classList.remove('puzzle');
+  });
+  upNextPuzzles[nextRandom];
 }
